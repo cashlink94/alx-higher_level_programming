a89bbcd730fc8daf7c609c06e10b3183e17423d0
cashlink94 <cashlink94@gmail.com>
2023-08-15 22:57:43 +0000

    Data Structures

--- 0x03-python-data_structures/100-test_lists.py
+++ 0x03-python-data_structures/100-test_lists.py
@@ -1,41 +1,22 @@
-import ctypes
+mport ctypes
 
-# Load the shared library
 lib = ctypes.CDLL('./libPyList.so')
-
-# Set argument types for the function
 lib.print_python_list_info.argtypes = [ctypes.py_object]
-
-# Create a Python list
-python_list = ['hello', 'World']
-
-# Display information about the list
-lib.print_python_list_info(python_list)
-
-# Modify the list
-	del python_list[1]
-lib.print_python_list_info(python_list)
-
-	python_list = python_list + [4, 5, 6.0, (9, 8), [9, 8, 1024], "Holberton"]
-lib.print_python_list_info(python_list)
-
-	python_list = []
-
-# Display information about an empty list
-lib.print_python_list_info(python_list)
-
-# Append elements to the list
-	python_list.append(0)
-	lib.print_python_list_info(python_list)
-	python_list.append(1)
-	python_list.append(2)
-	python_list.append(3)
-python_list.append(4)
-
-# Display information after appending elements
-lib.print_python_list_info(python_list)
-
-# Remove the last element from the list
-	python_list.pop()
-lib.print_python_list_info(python_list)
+l = ['hello', 'World']
+lib.print_python_list_info(l)
+del l[1]
+lib.print_python_list_info(l)
+l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "Holberton"]
+lib.print_python_list_info(l)
+l = []
+lib.print_python_list_info(l)
+l.append(0)
+lib.print_python_list_info(l)
+l.append(1)
+l.append(2)
+l.append(3)
+l.append(4)
+lib.print_python_list_info(l)
+l.pop()
+lib.print_python_list_info(l)
 
