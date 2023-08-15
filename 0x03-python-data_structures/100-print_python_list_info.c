d75526b32c8be2be7fc6d7f73f70dfbc5345225c
cashlink94 <cashlink94@gmail.com>
2023-08-15 23:37:02 +0000

    Data Structures

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,26 +1,26 @@
 #include <Python.h>
 
 /**
- * analyze_python_list - Provides essential insights about Python lists.
+ * analyze_python_list - Displays essential details about Python lists.
  * @list_object: A PyObject representing a list.
  */
 void analyze_python_list(PyObject *list_object)
 {
-	int list_size, allocation_size, index;
-	PyObject *list_item;
+    int list_size, allocation_size, index;
+    PyObject *list_item;
 
-	list_size = Py_SIZE(list_object);
-	allocation_size = ((PyListObject *)list_object)->allocated;
+    list_size = Py_SIZE(list_object);
+    allocation_size = ((PyListObject *)list_object)->allocated;
 
-	printf("[*] Size of the Python List = %d\n", list_size);
-	printf("[*] Allocated = %d\n", allocation_size);
+    printf("[*] Size of the Python List = %d\n", list_size);
+    printf("[*] Allocated = %d\n", allocation_size);
 
-	for (index = 0; index < list_size; index++)
-	{
-		printf("Element %d: ", index);
+    for (index = 0; index < list_size; index++)
+    {
+        printf("Element %d: ", index);
 
-		list_item = PyList_GetItem(list_object, index);
-		printf("%s\n", Py_TYPE(list_item)->tp_name);
-	}
+        list_item = PyList_GetItem(list_object, index);
+        printf("%s\n", Py_TYPE(list_item)->tp_name);
+    }
 }
 
